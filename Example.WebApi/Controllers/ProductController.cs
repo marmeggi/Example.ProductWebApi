8cbcea02212fdeb9d993e83807dc8407092166dd
DESKTOP-T6P1P5J\Silvana <marmeggi.silvana@gmail.com>
2023-05-26 10:51:39 +0200

    Revert "Made the listOfProducts static"
    This reverts commit ab93a939a8c648d2250bd97969948b5ddf53d700.

--- Example.WebApi/Controllers/ProductController.cs
+++ Example.WebApi/Controllers/ProductController.cs
@@ -15,7 +15,7 @@ namespace Example.WebApi.Controllers
 {
     public class ProductController : ApiController
     {
-        public static List<Product> listOfProducts;
+        public List<Product> listOfProducts;
 
         public ProductController()
         {
@@ -49,15 +49,7 @@ namespace Example.WebApi.Controllers
         }
 
         // POST api/product
-       /* public HttpResponseMessage Post([FromBody]  Product productToCreate)
-        {
-            listOfProducts.Add(productToCreate);
-
-            return Request.CreateResponse(HttpStatusCode.Created, listOfProducts);
-
-        }
-       */
-        public HttpResponseMessage Post([FromUri] Product productToCreate)
+        public HttpResponseMessage Post([FromBody]  Product productToCreate)
         {
             listOfProducts.Add(productToCreate);
 
